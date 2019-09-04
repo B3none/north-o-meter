f3f2bd5d300112c7c390637922fd2e51c0ff4134
⭐ B3none <ablackham2000@gmail.com>
2019-09-04 21:20:38 +0100

    Continued refactoring

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# North-o-meter
+# North-o-Meter
 The code, albeit slightly modified, for popular north-o-meter quiz!
--- web/js/game.js
+++ web/js/game.js
@@ -13,72 +13,89 @@ var ratings = [
 ];
 
 var height = 50;
-var questions =
-  [{
-    "q": "When did you last eat chips and gravy?",
-    "a0": "This week",
-    "e0": 10,
-    "a1": "This year",
-    "e1": 5,
-    "a2": "Longer, if ever",
-    "e2": -10
+var questions = [
+  {
+    q: 'When did you last eat chips and gravy?',
+    a0: 'This week',
+    e0: 10,
+    a1: 'This year',
+    e1: 5,
+    a2: 'Longer, if ever',
+    e2: -10
   },
-    {
-      "q": "Wheear 'ast tha bin sin' ah saw thee?\n",
-      "a0": "On Ilkla Mooar baht 'at\n",
-      "e0": 10,
-      "a1": "Wheear t'ducks play football",
-      "e1": 7,
-      "a2": "What?",
-      "e2": -10
-    },
-    {
-      "q": "What's an oatcake?",
-      "a0": "It's like an oaty pancake",
-      "e0": 5,
-      "a1": "It's like oaty flatbread",
-      "e1": 5,
-      "a2": "It's like porridge",
-      "e2": -5
-    },
-    {
-      "q": "When do you wear a coat?",
-      "a0": "When it's cold",
-      "e0": -5,
-      "a1": "When there's an R in the month",
-      "e1": -3,
-      "a2": "When hell freezes over",
-      "e2": 10
-    },
-    {"q": "In the evening, do you have...", "a0": "Dinner", "e0": -5, "a1": "Supper", "e1": -2, "a2": "Tea", "e2": 5},
-    {
-      "q": "Pie barm!",
-      "a0": "Gerrit in yer.",
-      "e0": 10,
-      "a1": "Death on a plate",
-      "e1": 5,
-      "a2": "I have no idea what that is",
-      "e2": -10
-    },
-    {
-      "q": "Someone talks to you on a bus. Do you...",
-      "a0": "Assume they want money",
-      "e0": -5,
-      "a1": "Politely ignore them",
-      "e1": -3,
-      "a2": "Chat back",
-      "e2": 10
-    },
-    {"q": "What are you drinking?", "a0": "Bitter", "e0": 5, "a1": "Mild", "e1": 3, "a2": "Pear cider", "e2": -10},
-    {
-      "q": "In the playground, did you play...",
-      "a0": "Tig or tiggy",
-      "e0": 5,
-      "a1": "Tag or tap",
-      "e1": -5,
-      "a2": "Had, hit or it",
-      "e2": -10
-    }];
+  {
+    q: 'Wheear \'ast tha bin sin\' ah saw thee?\n',
+    a0: 'On Ilkla Mooar baht \'at\n',
+    e0: 10,
+    a1: 'Wheear t\'ducks play football',
+    e1: 7,
+    a2: 'What?',
+    e2: -10
+  },
+  {
+    q: 'What\'s an oatcake?',
+    a0: 'It\'s like an oaty pancake',
+    e0: 5,
+    a1: 'It\'s like oaty flatbread',
+    e1: 5,
+    a2: 'It\'s like porridge',
+    e2: -5
+  },
+  {
+    q: 'When do you wear a coat?',
+    a0: 'When it\'s cold',
+    e0: -5,
+    a1: 'When there\'s an R in the month',
+    e1: -3,
+    a2: 'When hell freezes over',
+    e2: 10
+  },
+  {
+    q: 'In the evening, do you have...',
+    a0: 'Dinner',
+    e0: -5,
+    a1: 'Supper',
+    e1: -2,
+    a2: 'Tea',
+    e2: 5
+  },
+  {
+    q: 'Pie barm!',
+    a0: 'Gerrit in yer.',
+    e0: 10,
+    a1: 'Death on a plate',
+    e1: 5,
+    a2: 'I have no idea what that is',
+    e2: -10
+  },
+  {
+    q: 'Someone talks to you on a bus. Do you...',
+    a0: 'Assume they want money',
+    e0: -5,
+    a1: 'Politely ignore them',
+    e1: -3,
+    a2: 'Chat back',
+    e2: 10
+  },
+  {
+    q: 'What are you drinking?',
+    a0: 'Bitter',
+    e0: 5,
+    a1: 'Mild',
+    e1: 3,
+    a2: 'Pear cider',
+    e2: -10
+  },
+  {
+    q: 'In the playground, did you play...',
+    a0: 'Tig or tiggy',
+    e0: 5,
+    a1: 'Tag or tap',
+    e1: -5,
+    a2: 'Had, hit or it',
+    e2: -10
+  }
+];
 
 var question = {};
 
@@ -99,11 +116,11 @@ function setThermo() {
     {
       'top': (100 - height).toString() + '%'
     },
-    2000, "easeOutElastic");
+    2000, 'easeOutElastic');
 }
 
 function nextQuestion() {
-  question = questions.shift();
+  question = questions.splice(Math.floor(Math.random() * questions.length), 1);
   $('#game h2').text(question.q);
   $('#game button:eq(0)').text(question.a0);
   $('#game button:eq(1)').text(question.a1);
@@ -122,11 +139,12 @@ function handleAnswer(id) {
 
   setThermo();
 
-  if (questions.length > 0) {
+  if (questions.length) {
     nextQuestion();
-  } else {
-    gameOver();
+    return;
   }
+
+  gameOver();
 }
 
 function gameOver() {
